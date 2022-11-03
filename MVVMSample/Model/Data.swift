edaf640c2fc2a8814d678926eb49466cb07790aa
almiladurukavak <almiladurukavak@gmail.com>
2022-11-03 14:49:10 +0300

    first commit

---
+++ MVVMSample/Model/Data.swift
@@ -0,0 +1,12 @@
+ 
+import Foundation
+
+class Data {
+    var title: String
+    var subTitle: String
+    
+    init(title: String, subTitle: String) {
+        self.title = title
+        self.subTitle = subTitle
+    }
+}
---
+++ MVVMSample/View/CustomTableViewCell.swift
@@ -0,0 +1,9 @@
+ 
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblSubTitle: UILabel!
+    
+}
